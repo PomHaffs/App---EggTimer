c008410b0e3ce0f85f636b0db71bf903b3cd4bae
PomHaffs <tomashaffenden@icloud.com>
2016-12-13 15:49:54 +1100

    Added logic to egg timer

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -9,10 +9,57 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //this is now global
+    var timer = Timer()
+    
+    var currentNumberofSeconds = 210
+    
+    @IBOutlet weak var timerNumber: UILabel!
+    
+    @IBAction func pauseButtonPressed(_ sender: Any) {
+        timer.invalidate()
+    }
+    
+    @IBAction func playButtonPressed(_ sender: Any) {
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.countdownTimer), userInfo: nil, repeats: true)
+    }
+        
+    @IBAction func minusTenPressed(_ sender: Any) {
+        
+        if currentNumberofSeconds > 10 {
+           
+            currentNumberofSeconds -= 10
+            timerNumber.text = String(currentNumberofSeconds)
+        }
+        
+    }
+    
+    @IBAction func plusTenPressed(_ sender: Any) {
+        currentNumberofSeconds += 10
+        timerNumber.text = String(currentNumberofSeconds)
+    }
+    
+    @IBAction func refreshPressed(_ sender: Any) {
+        currentNumberofSeconds = 210
+        timerNumber.text = String(currentNumberofSeconds)
+    }
+    
+    
+    func countdownTimer() {
+        
+        if currentNumberofSeconds > 0 {
+            currentNumberofSeconds -= 1
+            timerNumber.text = String(currentNumberofSeconds)
+        } else {
+            timer.invalidate()
+        }
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
     }
 
     override func didReceiveMemoryWarning() {
